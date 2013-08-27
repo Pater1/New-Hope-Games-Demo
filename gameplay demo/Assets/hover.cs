0c4c11ea28b4294182d0c8bd4f44afed9deb4282
Pater1 <PConner000@gmail.com>
2013-08-27 16:17:58 -0700

    more stuff did

---
+++ gameplay demo/Assets/hover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class hover : MonoBehaviour {
+	
+	public float force = 10f;
+	
+	Random varX, varZ;
+	
+	int localX, localZ;
+	
+	bool moveEast = false, moveNorth = false;
+	
+	void OnTriggerStay (Collider other){
+		
+		other.rigidbody.AddForce(Vector3.up * force, ForceMode.Acceleration);
+			
+	}
+	
+}
